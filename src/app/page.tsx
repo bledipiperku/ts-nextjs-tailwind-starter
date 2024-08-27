cb62a66bd9166d9df6c3cce834b40c5d81b56982
Bledi Piperku <39462384+bledipiperku@users.noreply.github.com>
2024-08-27 22:01:11 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,8 +61,8 @@ export default function HomePage() {
 
           <footer className='absolute bottom-2 text-gray-700'>
             © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
+            <UnderlineLink href='https://piperku.com?ref=tsnextstarter'>
+              Bledi Piperku
             </UnderlineLink>
           </footer>
         </div>
