7e1b2347c8ca30d4f377132378c9934b983ffede
Bledi Piperku <39462384+bledipiperku@users.noreply.github.com>
2024-08-27 22:04:21 +0200

    Update config.ts

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,6 +2,6 @@ export const siteConfig = {
   title: 'Next.js + Tailwind CSS + TypeScript Starter',
   description:
     'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+  /** Without additional '/' on the end, e.g. https://piperku.com */
   url: 'https://tsnext-tw.thcl.dev',
 };
